873df277deca4a94bb3d46ecef49471ab42de9ac
Hoang Van Luyen <vanhoangluyen@gmail.com>
2017-11-05 11:06:32 +0700

    init

--- SplitUIImage/DetailViewController.swift
+++ SplitUIImage/DetailViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.delegate = self
+            scrollView.contentSize = detailImageView.frame.size
+            scrollView.minimumZoomScale = 1.0
+            scrollView.maximumZoomScale = 5.0
+        }
+    }
+    
+    var detailCandy: Candy? {
+        didSet {
+            configureView()
+        }
+    }
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        if let detail = detailCandy {
+            if let labelText = detailDescriptionLabel, let detailImage = detailImageView {
+                labelText.text = detail.name
+                detailImage.image = UIImage(named: detail.name)
+                title = detailCandy?.category
             }
         }
     }
@@ -32,14 +47,8 @@ class DetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.detailImageView
     }
-
-
 }
 
--- SplitUIImage/MasterViewController.swift
+++ SplitUIImage/MasterViewController.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 
+struct Candy {
+    let category: String
+    let name: String
+}
 class MasterViewController: UITableViewController {
-
+    var candies = [
+        Candy(category:"Chocolate", name:"Chocolate Bar"),
+        Candy(category:"Chocolate", name:"Chocolate Chip"),
+        Candy(category:"Chocolate", name:"Dark Chocolate"),
+        Candy(category:"Hard", name:"Lollipop"),
+        Candy(category:"Hard", name:"Candy Cane"),
+        Candy(category:"Hard", name:"Jaw Breaker"),
+        Candy(category:"Other", name:"Caramel"),
+        Candy(category:"Other", name:"Sour Chew"),
+        Candy(category:"Other", name:"Gummi Bear"),
+        Candy(category:"Other", name:"Candy Floss"),
+        Candy(category:"Chocolate", name:"Chocolate Coin"),
+        Candy(category:"Chocolate", name:"Chocolate Egg"),
+        Candy(category:"Other", name:"Jelly Beans"),
+        Candy(category:"Other", name:"Liquorice"),
+        Candy(category:"Hard", name:"Toffee Apple")]
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -37,21 +53,15 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
 
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let candy = candies[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailCandy = candy
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -65,14 +75,15 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return candies.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let candy = candies[indexPath.row]
+        cell.textLabel!.text = candy.name
+        cell.detailTextLabel?.text = candy.category
         return cell
     }
 
@@ -83,7 +94,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+            candies.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
